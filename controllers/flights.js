ae3dff021b8ced2ad1ef68d07afba7a8533172e7
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-17 21:52:35 -0500

    stub up controller functions, initialize views

---
+++ controllers/flights.js
@@ -0,0 +1,33 @@
+import { Flight } from '../models/flight.js'
+
+function newFlight(req, res) {
+  console.log(req.body)
+}
+
+function create(req, res) {
+
+}
+
+function index(req, res) {
+
+}
+
+function show(req, res) {
+
+}
+
+function edit(req, res) {
+
+}
+
+
+
+
+
+export {
+  newFlight as new,
+  create,
+  index,
+  show,
+  edit
+}
---
+++ models/flight.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  aiport: {
+    type: String,
+    enum: ['AUS', 'DFQ', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN'
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999,
+    required: true
+  },
+  departs: {
+    type: Date,
+    default: function () {
+      return new Date().getFullYear()
+    }
+  }
+})
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+
+export {
+  Flight
+}
